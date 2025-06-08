57e342c6ac482035d1f4860b148aa32dd2a88a6b
JupiterIT5 <geno74@bk.ru>
2025-06-08 20:36:37 +0500

    update api

--- lib/redux/api/Categories/CategoriesApi.ts
+++ lib/redux/api/Categories/CategoriesApi.ts
@@ -33,7 +33,7 @@ export const CategoriesApi = createApi({
 		}),
 		hiddenCategory: builder.mutation<void, IHiddenCategory>({
 			query: ({id, serviceId, token }) => ({
-				url: `/service-categories/${serviceId}/subservices/${id}/hidden`,
+				url: `/service-categories/${serviceId}/hidden`,
 				method: 'PATCH',
 				headers: {
 					Authorization: `Bearer ${token}`,
@@ -42,7 +42,7 @@ export const CategoriesApi = createApi({
 		}),
 		visibleCategory: builder.mutation<void, IHiddenCategory>({
 			query: ({id, serviceId, token }) => ({
-				url: `/service-categories/${serviceId}/subservices/${id}/visible`,
+				url: `/service-categories/${serviceId}/visible`,
 				method: 'PATCH',
 				headers: {
 					Authorization: `Bearer ${token}`,
