c933ea16687130c5bcfae63eac0df8fc6eb68d63
Nextily <geno74arhipov@gmail.com>
2025-06-12 11:48:29 +0500

    Update page.tsx

--- app/(page)/auth/signin/page.tsx
+++ app/(page)/auth/signin/page.tsx
@@ -40,7 +40,7 @@ const SignIn = () => {
 					router.push('/dashboard/statements')
 				}, 2500)
 			} else {
-				console.log(response.error)
+				console.log(response)
 				alert(response.error!.message!)
 				myToast({ message: 'Неверные данные!', type: 'error' })
 			}
